2792100f14d32fe3850a4a97a600906e99d158e6
Brayan <brayan1897mendez@gmail.com>
2023-03-08 23:20:53 -0500

    Se agrega manejo de estado de componentes
    -Filtrar los todos para aparecer o no, dependiendo de lo que se escriba en el componente TodoSearch
    - Se crean métodos para marcar como realizada la tarea o eliminarla.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,69 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
-const todos = [
-  { text: "Cortar cebolla", completed: false },
+
+const defaultTodos = [
+  { text: "Cortar cebolla", completed: true },
   { text: "Tomar el cursso de intro a React", completed: true },
   { text: "Llorar con la llorona", completed: false },
-  { text: "LALALALAA", completed: false },
+  { text: "Dormir", completed: false },
 ];
+
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos); // se crea un estado guardago en la variable todos, con el set se puede actualizar la funcion
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length; //filtrar si cada todo tiene la propiedad todo.completed como true
+  const totalTodos = todos.length;
+
+  //se filtran los todos para que aparezcan o no, dependiendo lo que escriban en el componente TodoSearch:
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      //se llama a cada todo, y por cada uno se convierte el texto en minuscula para analizarlo
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText); // se filtra cual de los todos incluye el texto escrito en el input de busqueda
+    });
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text); // por cada todo analizado ver si es igual a text (se recibe en la funcion)
+    const newTodos = [...todos]; //crea nueva lista de todos
+    newTodos[todoIndex] = {
+      text: todos[todoIndex].text,
+      completed: true, //marcar el todo que cumple con lo anterior con la propiedad true
+    };
+    //todos[todoIndex].completed = true; (forma corta de lo anterior)
+    setTodos(newTodos); //actualiza el estado para reenderizar
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    {
+      newTodos.splice(todoIndex, 1); //este metodo agarra la funcion o posicion y elimina la cantidad que se le diga
+    }
+    setTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-  return <input className="TodoSearch" placeholder="Cebolla" />;
+function TodoSearch({ searchValue, setSearchValue }) {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value); // muestra los valores buscados
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      value={searchValue} // el valor del input es igual al valor guardado en el estado
+      onChange={onSearchValueChange} //metodo//propiedad
+    />
+  );
 }
 
 export { TodoSearch };
